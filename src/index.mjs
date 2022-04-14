e5d3b9b835996224ee6a1fe574dbda3936221eeb
Marcello Faria <01faria.marcello@gmail.com>
2022-04-15 01:47:00 +0700

    add animation frame

--- src/index.mjs
+++ src/index.mjs
@@ -210,28 +210,34 @@ function init() {
   document.getElementById("slider0").onchange = function (event) {
     theta[torsoId] = event.target.value;
     initNodes(torsoId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider1").onchange = function (event) {
     theta[head1Id] = event.target.value;
     initNodes(head1Id);
+    requestAnimationFrame(render);
   };
 
   document.getElementById("slider2").onchange = function (event) {
     theta[leftUpperArmId] = event.target.value;
     initNodes(leftUpperArmId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider3").onchange = function (event) {
     theta[leftLowerArmId] = event.target.value;
     initNodes(leftLowerArmId);
+    requestAnimationFrame(render);
   };
 
   document.getElementById("slider4").onchange = function (event) {
     theta[rightUpperArmId] = event.target.value;
     initNodes(rightUpperArmId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider5").onchange = function (event) {
     theta[rightLowerArmId] = event.target.value;
     initNodes(rightLowerArmId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider6").onchange = function (event) {
     theta[leftUpperLegId] = event.target.value;
@@ -240,18 +246,22 @@ function init() {
   document.getElementById("slider7").onchange = function (event) {
     theta[leftLowerLegId] = event.target.value;
     initNodes(leftLowerLegId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider8").onchange = function (event) {
     theta[rightUpperLegId] = event.target.value;
     initNodes(rightUpperLegId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider9").onchange = function (event) {
     theta[rightLowerLegId] = event.target.value;
     initNodes(rightLowerLegId);
+    requestAnimationFrame(render);
   };
   document.getElementById("slider10").onchange = function (event) {
     theta[head2Id] = event.target.value;
     initNodes(head2Id);
+    requestAnimationFrame(render);
   };
 
   document.getElementById("colorpicker").addEventListener("change", function (e) {
