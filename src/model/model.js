91a801a9bdf167b56be5e8480cf3c61b3eb74e50
Dwi Bagus <dwibagus154@gmail.com>
2022-04-19 06:59:18 +0700

    feat: add model

---
+++ src/model/model.js
@@ -0,0 +1,39 @@
+var torsoHeight = 8.0;
+var torsoWidth = 3.0;
+var upperArmHeight = 5.0;
+var lowerArmHeight = 2.0;
+var upperArmWidth = 1.3;
+var lowerArmWidth = 0.8;
+var upperLegWidth = 1.3;
+var lowerLegWidth = 0.8;
+var lowerLegHeight = 2.0;
+var upperLegHeight = 5.0;
+var headHeight = 3.5;
+var headWidth = 1.5;
+var neckHeight = 4.0;
+var neckWidth = 2.0;
+
+var torsoHeight2 = 5.0;
+var torsoWidth2 = 1.0;
+var upperArmHeight2 = 3.0;
+var lowerArmHeight2 = 2.0;
+var upperArmWidth2 = 0.5;
+var lowerArmWidth2 = 0.5;
+var upperLegWidth2 = 0.5;
+var lowerLegWidth2 = 0.5;
+var lowerLegHeight2 = 2.0;
+var upperLegHeight2 = 3.0;
+var headHeight2 = 1.5;
+var headWidth2 = 1.0;
+
+var numNodes = 11;
+var numAngles = 11;
+var angle = 0;
+
+var initTheta = [
+  90, 120, 90, 70, 10, 80, 10, 90, 40, 70, 30, 0, -90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0, 120,
+];
+var theta = [
+  90, 120, 90, 70, 10, 80, 10, 90, 40, 70, 30, 0, -90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0, 120,
+];
+var theta2 = [0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0, 120];
