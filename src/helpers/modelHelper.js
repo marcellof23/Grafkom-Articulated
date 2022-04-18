e46d3a37748d56b95a9f67dd4089061cd90b4800
Dwi Bagus <dwibagus154@gmail.com>
2022-04-18 13:03:57 +0700

    fix: load data

--- src/helpers/modelHelper.js
+++ src/helpers/modelHelper.js
@@ -29,45 +29,41 @@ var leftLowerLegId2 = 28;
 var rightUpperLegId2 = 29;
 var rightLowerLegId2 = 30;
 
-var torsoHeight = 8.0;
-var torsoWidth = 3.0;
-var upperArmHeight = 5.0;
-var lowerArmHeight = 2.0;
-var upperArmWidth = 1.3;
-var lowerArmWidth = 0.8;
-var upperLegWidth = 1.3;
-var lowerLegWidth = 0.8;
-var lowerLegHeight = 2.0;
-var upperLegHeight = 5.0;
-var headHeight = 3.5;
-var headWidth = 1.5;
-var neckHeight = 4.0;
-var neckWidth = 2.0;
+var torsoHeight;
+var torsoWidth;
+var upperArmHeight;
+var lowerArmHeight;
+var upperArmWidth;
+var lowerArmWidth;
+var upperLegWidth;
+var lowerLegWidth;
+var lowerLegHeight;
+var upperLegHeight;
+var headHeight;
+var headWidth;
+var neckHeight;
+var neckWidth;
 
-var torsoHeight2 = 5.0;
-var torsoWidth2 = 1.0;
-var upperArmHeight2 = 3.0;
-var lowerArmHeight2 = 2.0;
-var upperArmWidth2 = 0.5;
-var lowerArmWidth2 = 0.5;
-var upperLegWidth2 = 0.5;
-var lowerLegWidth2 = 0.5;
-var lowerLegHeight2 = 2.0;
-var upperLegHeight2 = 3.0;
-var headHeight2 = 1.5;
-var headWidth2 = 1.0;
+var torsoHeight2;
+var torsoWidth2;
+var upperArmHeight2;
+var lowerArmHeight2;
+var upperArmWidth2;
+var lowerArmWidth2;
+var upperLegWidth2;
+var lowerLegWidth2;
+var lowerLegHeight2;
+var upperLegHeight2;
+var headHeight2;
+var headWidth2;
 
-var numNodes = 11;
-var numAngles = 11;
-var angle = 0;
+var numNodes;
+var numAngles;
+var angle;
 
-var initTheta = [
-  90, 120, 90, 70, 10, 80, 10, 90, 40, 70, 30, 0, -90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0, 120,
-];
-var theta = [
-  90, 120, 90, 70, 10, 80, 10, 90, 40, 70, 30, 0, -90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0, 120,
-];
-var theta2 = [0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0, 120];
+var initTheta = [];
+var theta = [];
+var theta2 = [];
 
 var stack = [];
 
@@ -140,7 +136,7 @@ function setUpVariable(myJSON){
 }
 
 
-function initNodes(Id, myJSON) {
+function initNodes(Id) {
   var m = matrix4();
 
   // setUpVariable(myJSON)
